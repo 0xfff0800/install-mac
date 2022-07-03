1a69fb932b915bbf05b5ba4309091428cf0770d5
0x <55315423+0xfff0800@users.noreply.github.com>
2022-07-03 13:15:01 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,15 +1,23 @@
 echo $red 
 echo ""----------------------------------------------------------------------------- ""
-echo "install shell Mac OS"
+echo "install docker"
 echo "---------------------------------------------------------------------------------"
 
 echo
 
-sudo curl -sSL https://get.docker.com/ | sh
+sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository ""deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt-cache policy docker-ce
-apt install docker-ce
-docker run -it --device /dev/kvm -p5910:5900 -p 50922:10022 -v /tmp/.X11-unix:/tmp/.X11-unix -e ""DISPLAY=${DISPLAY:-:0.0}"" -e GENERATE_UNIQUE=true -e TERMS_OF_USE=i_agree -e EXTRA='-vnc :0' sickcodes/docker-osx:auto
+sudo apt install docker-ce
+
+echo
+
+
+echo ""----------------------------------------------------------------------------- ""
+echo "install shell Mac OS"
+echo "---------------------------------------------------------------------------------"
+
+sudo docker run -it --device /dev/kvm -p5910:5900 -p 50922:10022 -v /tmp/.X11-unix:/tmp/.X11-unix -e "DISPLAY=${DISPLAY:-:0.0}" -e GENERATE_UNIQUE=true -e TERMS_OF_USE=i_agree -e EXTRA='-vnc :0' sickcodes/docker-osx:auto
 
 exit
